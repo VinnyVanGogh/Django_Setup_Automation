1b37a012f6bc26f96a0c6527cf0d81bae214b0c0
VinnyVanGogh <133192356+VinnyVanGogh@users.noreply.github.com>
2023-10-12 08:00:33 -0700

    remove cache

--- PYTHON/add_base_urls.py
+++ PYTHON/add_base_urls.py
@@ -11,13 +11,11 @@ urlpatterns = [
 """.format(app_name))
 
 if __name__ == "__main__":
-    # Specify the app names and paths to their respective urls.py files
     apps = [
         ("api", "backend/api/urls.py"),
         ("core", "backend/core/urls.py"),
         ("utilities", "backend/utilities/urls.py"),
     ]
 
-    # Rewrite the urls.py files for each app
     for app_name, urls_path in apps:
         rewrite_settings(app_name, urls_path)
